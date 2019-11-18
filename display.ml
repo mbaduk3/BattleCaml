2cdc4d23a84fb5318928de5fcee22b080ac94ea9
Maxim Baduk <mb2474@cornell.edu>
2019-11-18 11:43:47 -0500

    render func

--- display.ml
+++ display.ml
@@ -4,23 +4,27 @@ open Gameboard
 let scr = ref (initscr ());
 ignore(Curses.cbreak ());
 ignore(Curses.noecho ())
-let b_win = ref (newwin 11 11 1 1)
-let cur_x = ref 0
-let cur_y = ref 0
+let b_win = ref (newwin 12 12 1 10)
+let cur_x = ref 1
+let cur_y = ref 1
 
 let hit_ch = int_of_char 'X'
 let miss_ch = int_of_char '0'
+let unhit_ch = int_of_char '~'
 let empty_ch = int_of_char '.'
 
 let incr_cur b = 
     cur_x := !cur_x + 1;
     if (!cur_x > Array.length b.(0)) then 
-        cur_x := 0;
-        cur_y := !cur_y + 1
+        let () = cur_y := !cur_y + 1 in 
+        cur_x := 1
+    else if (!cur_y > Array.length b) then 
+        cur_y := 1
+        
 
 let render_board b win = 
-    cur_x := 0;
-    cur_y := 0;
+    cur_x := 1;
+    cur_y := 1;
     for i = 0 to Array.length b - 1 do 
         for j = 0 to (Array.length b.(0) - 1) do 
             begin
@@ -31,6 +35,9 @@ let render_board b win =
             | Miss -> 
                 ignore(Curses.mvwaddch win !cur_y !cur_x miss_ch);
                 incr_cur b 
+            | Unhit -> 
+                ignore(Curses.mvwaddch win !cur_y !cur_x unhit_ch);
+                incr_cur b
             | _ -> 
                 ignore(Curses.mvwaddch win !cur_y !cur_x empty_ch);
                 incr_cur b
