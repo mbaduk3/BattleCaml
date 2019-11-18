bd15af0cbd6e8dd576fb9b405c1d78ce745d5f3c
Alexander Vaziri <av474@cornell.edu>
2019-11-18 13:45:18 -0800

    Add files via upload
    key inputs -> command

---
+++ command.ml
@@ -0,0 +1,23 @@
+open Curses
+
+type command = Fire | Rotate | Quit | Save | Up | Down | Left | Right | Other
+
+let width = 10
+let height = 10
+let start_x win = ref (snd (getyx win))
+let start_y win = ref (fst (getyx win))
+
+(* let key_pad = keypad scr true
+
+let no_delay = nodelay scr true *)
+
+let key_inputs win = 
+  match char_of_int (wgetch win) with
+  | 'S' | 's' -> Down 
+  | 'W' | 'w' -> Up
+  | 'A' | 'a' -> Left
+  | 'D' | 'd' -> Right
+  | 'F' | 'f' -> Fire
+  | 'R' | 'r' -> Rotate
+  | 'Q' | 'q' -> Quit
+  | _ -> Save
