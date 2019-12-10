dfd1ae35f382e6dc4189ce6d3cfbd96f27a6f4be
Alex <av474@cornell.edu>
2019-12-10 05:13:27 -0500

    lots of stuff

---
+++ ai_hard.ml
@@ -0,0 +1,128 @@
+open Gameboard
+open Random
+
+let empty_lst = ref []
+let ship_lst = ref []
+let fire_lst = [true; false]
+
+(* [determine_hard_fire ()] has a 50% chance or returning true and a 50% chance
+of returning false. *)
+let determine_hard_fire () = 
+  let elt = Random.int 2 in
+  List.nth fire_lst elt
+
+type orientation = Vertical | Horizontal
+
+(* [get_all_empty_coords miss_coords] returns a list of coordinate pairs representing
+the coordinates of type Empty. *)
+let get_all_empty_coords m = 
+  for i = 0 to Array.length m - 1 do
+    for j = 0 to Array.length m.(i) - 1 do
+      match m.(i).(j) with
+        | Empty ->  empty_lst := (i, j)::!empty_lst
+        | _ -> ()
+    done
+  done;
+  empty_lst
+
+(* [remove_empty coord reflst] *)
+let remove_empty coord reflst = 
+  reflst := List.filter (fun lst -> if lst = coord then false else true) !reflst
+
+(* [get_empty_coord miss_coords] returns a list of coordinate pairs representing
+the coordinates of type Empty. *)
+let get_empty_coord miss_coords = 
+  let rnd_ind = Random.int (List.length !miss_coords) in
+  let empty_coord = List.nth !miss_coords rnd_ind in
+  remove_empty empty_coord empty_lst;
+  empty_coord
+
+(* [thd tup] returns the third element of [tup], which is represented by 
+(x, y, len, orientation), thus returning the value of len *)
+let thd tup = 
+  match tup with
+    | (_, _, t, _) -> t
+
+(* [create_vertical_lst tup acc num] transforms [tup] into a list of coordinate pairs
+that describe the position of each of the user's placed vertical ships.*)
+let rec create_vertical_lst tup acc num = 
+  match num with
+    | num when num < (thd tup) -> 
+      let (x, y, _, _) = tup in (create_vertical_lst tup ((x, y+num)::acc) (succ num))
+    | _ -> ref (List.rev acc)
+
+(* [create_horizontal_lst tup acc num] transforms [tup] into a list of coordinate pairs
+that describe the position of each of the user's placed horizontal ships.*)
+let rec create_horizontal_lst tup acc num = 
+  match num with
+    | num when num < (thd tup) -> 
+      let (x, y, _, _) = tup in create_horizontal_lst tup ((x+num, y)::acc) (succ num)
+    | _ -> ref (List.rev acc)
+
+(* [ship_coordinates arr_to_lst acc] returns a reference of [arr_to_lst] whose elements
+are references to coordinate positions of the user's placed ships *)
+let rec ship_coordinates arr_to_lst acc = 
+    match arr_to_lst with
+      | [] -> ref acc
+      | h::t -> let (_, _, _, orientation) = h in
+                if orientation = Vertical then
+                  (ship_coordinates t ((create_vertical_lst h [] 0)::acc))
+                else
+                  (ship_coordinates t ((create_horizontal_lst h [] 0)::acc))
+
+(* [current_ship_index] is a reference to a value between 0 and 4 inclusive*)
+let current_ship_index = ref (Random.int 5)
+
+(* [new_index bound] is an integer to represent a new index into the list of ships
+to be fired at. *)
+let rec new_index bound = 
+  if bound = 1 then
+    0
+  else
+    let x = Random.int bound in
+    if x = !current_ship_index then
+      new_index bound
+    else 
+      x
+
+(* lst will be current lst being fired at *)
+let remove_coord_from_ship shipref coord = 
+  shipref := List.filter (fun c -> if c = coord then false else true) !shipref
+
+let remove_ship_if_empty () = 
+  ship_lst := List.filter (fun reflst -> if !reflst = [] then false else true) !ship_lst
+
+let change_ship_index_if_empty () = 
+  if !(List.nth !ship_lst (!current_ship_index)) = [] then 
+  begin
+    remove_ship_if_empty ();
+    let len = List.length (!ship_lst) in
+    current_ship_index := new_index len; 
+    !current_ship_index
+  end
+  else
+    !current_ship_index
+
+(* bangships should be !ship_lst *)
+let rec ai_win_condition bangships = 
+  match bangships with
+    | [] -> true
+    | h::t -> if !h = [] then
+                ai_win_condition t 
+              else
+                false
+  
+(* Returns coordinate to fire at. arr is an array of coordinates *)
+let get_coord_of_hit lst =
+  let rnd_ind = Random.int (List.length lst) in
+  List.nth lst rnd_ind
+
+let ai_fire m = 
+  let curr_ship = !(List.nth !ship_lst (change_ship_index_if_empty ())) in
+  match determine_hard_fire () with
+    | true -> let (x, y) = (get_coord_of_hit curr_ship) in
+              m.(x).(y) <- Hit;
+              Contact m
+    | false -> let (x, y) = get_empty_coord (get_all_empty_coords m) in
+               m.(x).(y) <- Miss;
+               No_contact m
