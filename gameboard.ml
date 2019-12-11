83211f14f6807f51cfcf8f30bbebd52bf9aa7d43
Maxim Baduk <mb2474@cornell.edu>
2019-12-11 16:21:49 -0500

    gameboard docs

--- gameboard.ml
+++ gameboard.ml
@@ -1,38 +1,25 @@
-(* This file will have all of the gameboard mechanics. *)
-
 type coord = (int * int)
-
 type powerup = Sea_mine | Bomb | Double_bullet | Points | Repair_kit
-
 type entry = Hit | Miss | Unhit | Empty | Collected | Uncollected of powerup
-
 type t = entry array array
-
 type ship = t
-
 type response = Contact of t | No_contact of t | 
                 Already_hit of t | Already_miss of t | Misc
-
 type orientation = Vertical | Horizontal
 
 exception Malformed 
 exception Out_of_bounds
 
-(* Note to self: 
-   For Array.make_matrix x y elt, we would read it as x rows of length y whose
-   elements are elt.
-   Array.(i).(j) gives the val at row i, column j *)
-
-(* A 10x10 board of Empty values *)
+(** [init_matrix ()] is a 2-dimensional 10x10 array of [Empty] values *)
 let init_matrix () = Array.make_matrix 10 10 Empty
 
-(* Returns the 0-based index of [elem] in the list [lst] *)
+(** [index lst elem acc] is the 0-based index of [elem] in the list [lst] *)
 let rec index lst elem acc = 
   match lst with
   | [] -> acc
   | h::t -> if h = elem then acc else index t elem (succ acc)
 
-(* [get_array_from i j] is a copy of [arr], but indexed from [i] to 
+(** [get_array_from i j] is a subset of [arr], indexed from [i] to 
    [j]-non-inclusive. *)
 let get_array_from i j arr = 
   let lst = Array.to_list arr in 
@@ -44,9 +31,7 @@ let get_array_from i j arr =
        else array_match i j t acc (succ num)) in
   Array.of_list (array_match i j lst [] 0)
 
-(** A ship will always have 1 or n elements in the matrix. 1 if horizontal. n if vertical*)
-
-(* Creates a new "ship" Board of Unhit elements of length [len]. *)
+(** [create_ship len] is an array of [Unhit] values of length [len]. *)
 let create_ship len = Array.make len Unhit
 
 (* The standard game ship suite *)
@@ -57,6 +42,7 @@ let caml_3 = ships.(2) <- (create_ship 3, Horizontal)
 let caml_3' = ships.(3) <- (create_ship 3, Horizontal)
 let caml_2 = ships.(4) <- (create_ship 2, Horizontal)
 
+(* The standard game opponent ship suite *)
 let opp_ships = Array.make 5 (Array.make 0 Unhit, Horizontal)
 let opp_5 = opp_ships.(0) <- (create_ship 5, Horizontal)
 let opp_4 = opp_ships.(1) <- (create_ship 4, Horizontal)
@@ -68,7 +54,8 @@ let hard_mode_powerups = Sea_mine :: Bomb :: Double_bullet
                          :: Points :: Repair_kit :: []
 let easy_mode_powerups = hard_mode_powerups @ hard_mode_powerups
 
-(* The single-character representation of Entry [e]. *)
+(** [string_of_entry e] is the one-character string representation of 
+    Entry [e]. *)
 let string_of_entry e = 
   match e with 
   | Hit -> "H"
@@ -78,10 +65,10 @@ let string_of_entry e =
   | Empty -> "." 
   | Uncollected p -> "."
 
-(* Returns n % m, handling negative numbers *)
+(** [new_mod n m] is n % m, handling negative numbers *)
 let new_mod n m = (n + m) mod m
 
-(* Returns the row at index [num] in matrix [m] *)
+(** [get_row n num] is the row at index [num] in matrix [m] *)
 let get_row m num = m.(num)
 
 let demo_board = 
@@ -90,13 +77,13 @@ let demo_board =
 let nuke_board = 
   Array.make_matrix 10 10 Hit
 
-(* Returns a new matrix where the rows of [m] become the columns of 
-   [transpose m] *)
+(* [transpose m] is the transpose of matrix [m] *)
 let transpose m = 
   Array.init (Array.length m.(0)) (fun i -> 
       Array.init (Array.length m) (fun j -> m.(j).(i)))
 
-(* Returns the value at the x, y coordinates contained in [c], of matrix [m] *)
+(** [get_val_of_coord m c] is the value at coordinates x, y contained in [c], 
+    of matrix [m] *)
 let get_val_of_coord (m:t) (c:coord) = m.(fst c).(snd c)
 
 let check_explosion x y m =
@@ -164,8 +151,8 @@ let handle_powerup c m p =
       check_explosion (fst c + 1) (snd c + 1) m;
     end
 
-(* Changes the board [m] based on the entry value at coordinates [c]. Returns 
-   a response type containing the new board [fire c m]. *)
+(** [fire c m] modifies the board [m] based on the entry value at coordinates [c]. 
+   Returns a response type containing the new board. *)
 let fire (c:coord) m = 
   match get_val_of_coord m c with
   | Empty -> m.(fst c).(snd c) <- Miss; No_contact m
@@ -179,6 +166,7 @@ let fire (c:coord) m =
 let second_elt lst = List.nth lst 1
 let third_elt lst = List.nth lst 2
 
+(** [string_of_tuple tup] is a string representation of tuple [tup]. *)
 let string_of_tuple tup = 
   let x = fst tup in 
   let y = snd tup in
@@ -192,12 +180,12 @@ let give_hint matrix =
       print_string "\nCouldn't Find Anything For You. Just Keep Firing!\n"
   done
 
+(** [format_row row] prints the elements of array [row] to the console *)
 let format_row (row: entry array) = 
   Array.iter (fun elem -> print_string (string_of_entry elem)) row;
   print_string "\n"
 
+(** [format board] prints the elements of Board [board] to the console *)
 let format (board:t) = 
   print_string "\n";
   Array.iter format_row board
-
-
