ef8845eab9b52419165207bce5009b35b578906e
Maxim Baduk <mb2474@cornell.edu>
2019-12-11 19:19:14 -0500

    mlis

---
+++ ascii.mli
@@ -0,0 +1,22 @@
+(**
+    This module stores all of the ascii art used in various screens of the game.
+    All of the art is stored as strings.
+ *)
+
+ val battlecaml_str : string
+
+ val p_to_play_str : string
+
+ val p_to_play_2_str : string
+
+ val camel1_str : string
+
+ val camel_menu_str : string
+
+ val camel2_str : string
+
+ val camel3_str : string
+
+ val win_msg : string
+
+ val lose_msg : string
